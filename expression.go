813f52af6af086b80f06f69d9f08e57a753246ee
pacheng <pablo.cheng@cn.ey.com>
2024-11-26 01:17:58 +0800

    init parse

--- expression.go
+++ expression.go
@@ -2,6 +2,12 @@ package EasyExpression
 
 import "fmt"
 
+type match_Scope struct {
+    ChildrenExpressionString string
+    EndIndex int
+    Status bool
+}
+
 type expression struct {
 	//错误消息
 	ErrorMessgage string
@@ -34,6 +40,11 @@ func CreateExpression(expressionStr string) (*expression, error) {
 		SourceExpressionString: expressionStr,
 		DataString:             "",
 	}
+	defer func ()  {
+		if err := recover();err != nil{
+			exp.ErrorMessgage = err.(error).Error()
+		}
+	}()
 	if tryParse(&exp) {
 		return &exp, nil
 	}
@@ -41,6 +52,206 @@ func CreateExpression(expressionStr string) (*expression, error) {
 }
 
 func tryParse(exp *expression) bool {
-	//todo
+	parse(exp)
 	return true
+}
+
+func IsOver(expressionString string)bool{
+    if len(expressionString) == 0{
+        return true;
+    }else{
+        return !(Contains(expressionString, '(')|| Contains(expressionString, '[')|| Contains(expressionString, '&')|| Contains(expressionString, '|')|| Contains(expressionString, '!')|| Contains(expressionString, '>')|| Contains(expressionString, '<')|| Contains(expressionString, '=')|| Contains(expressionString, '+')|| Contains(expressionString, '-')|| Contains(expressionString, '*')|| Contains(expressionString, '/')|| Contains(expressionString, '%'));
+    }
+}
+
+func Contains(text string, contains byte)bool{
+    var lastChar byte
+    byteArray := []byte(text)
+    for i := 0; i < len(byteArray); i++{
+        if (text[i] == contains){
+            if lastChar != '\\'{
+                return true
+                } 
+        }
+        lastChar = byteArray[i];
+    }
+    return false;
+}
+
+func SetMatchMode(currentChar byte, lastMode MatchMode)(matchMode MatchMode, endTag byte){
+    //go里没有nullable类型，常量又不能取地址，所以此处用空格字符代替nil
+	switch currentChar{
+		case '(':
+			return Match_Mode_Scope, ')'
+		case '"':
+			return Match_Mode_Scope, '"'
+		case '\'':
+			return Match_Mode_Scope, '\''
+		case '[':
+			return Match_Mode_Function, ']'
+		case '&':
+			return Match_Mode_LogicSymbol, ' ';
+		case '|':
+			return Match_Mode_LogicSymbol, ' ';
+		case '!':
+			return Match_Mode_LogicSymbol, ' ';
+		case '+':
+			return Match_Mode_ArithmeticSymbol, ' ';
+		case '-':
+			//有可能是负号，也有可能是减号;上一个block是符号或者none，这此处应该当作负号处理
+			if lastMode == Match_Mode_Unknown || lastMode == Match_Mode_ArithmeticSymbol || lastMode == Match_Mode_LogicSymbol || lastMode == Match_Mode_RelationSymbol{
+				return Match_Mode_Data, ' '
+			}
+			return Match_Mode_ArithmeticSymbol, ' '
+		case '*':
+			return Match_Mode_ArithmeticSymbol, ' '
+		case '/':
+			return Match_Mode_ArithmeticSymbol, ' '
+		case '%':
+			return Match_Mode_ArithmeticSymbol, ' '
+		case '<':
+			return Match_Mode_RelationSymbol, ' '
+		case '>':
+			return Match_Mode_RelationSymbol, ' '
+		case '=':
+			/*=继承上一个相邻符号的类型，比如<=,>=，此时=号为关系运算符；上一个为逻辑运算符的话，此处=为逻辑运算符，比如 !=；如果上一个block不为符号，那么此时=为等于（关系运算符）
+			 因此，只有上一个block为逻辑运算符时，才返回logicSymbol，其他情况返回relationSymbol
+			 */
+			if lastMode == Match_Mode_LogicSymbol{
+				return Match_Mode_LogicSymbol, ' '
+			}
+			return Match_Mode_RelationSymbol, ' '
+		case '\\':
+			return Match_Mode_EscapeCharacter, ' '
+		default:
+			return Match_Mode_Data, ' '
+	}
+}
+
+func parse(exp *expression){
+    lastBlock := Match_Mode_Unknown;
+    for index := 0; index < len(exp.SourceExpressionString); index++{
+        var matchScope match_Scope
+        currentChar := exp.SourceExpressionString[index];
+        mode, endTag := SetMatchMode(currentChar, lastBlock);
+        switch mode {
+            case Match_Mode_Scope:
+                if currentChar == endTag{
+                    //'' 或者 "" 实际上应该认作数据类型
+                    matchScope = FindEnd(currentChar, expression.SourceExpressionString, index);
+                    tempStr := currentChar + matchScope.ChildrenExpressionString + endTag
+                    var dataExp,err = CreateExpression(tempStr)
+                    if err != nil{
+                        panic(err)
+                    }
+                    dataExp.DataString = matchScope.ChildrenExpressionString
+                    dataExp.ElementType = Element_Data
+                    expression.ExpressionChildren = append(expression.ExpressionChildren,dataExp)
+                    lastBlock = Match_Mode_Data;
+                    index = matchScope.EndIndex;
+                    continue;
+                }else
+                {
+                    matchScope = FindEnd(currentChar, endTag, expression.SourceExpressionString, index);
+                }
+                expression.Status = matchScope.Status;
+                break;
+            case Match_Mode_RelationSymbol:
+                var relationSymbolStr = GetFullSymbol(expression.SourceExpressionString, index, mode);
+                //去除可能存在的空字符
+                var relationSymbol = ConvertOperator(relationSymbolStr.Replace(" ", ""));
+                expression.Operators.Add(relationSymbol);
+                expression.ElementType = ElementType.Expression;
+                //如果关系运算符为单字符，则索引+0，如果为多字符（<和=中间有空格，需要忽略掉），则跳过这段。eg: <；<=；<  =；
+                index += relationSymbolStr.Length - 1;
+                lastBlock = mode;
+                continue;
+            case Match_Mode_LogicSymbol:
+                var logicSymbolStr = GetFullSymbol(expression.SourceExpressionString, index, mode);
+                var logicSymbol = ConvertOperator(logicSymbolStr.Replace(" ", ""));
+                //因为! 既可以单独修饰一个数据，当作逻辑非，也可以与=联合修饰两个数据，当作不等于，所以此处需要进行二次判定。如果是!=，则此符号为关系运算符
+                expression.Operators.Add(logicSymbol);
+                expression.ElementType = ElementType.Expression;
+                index += logicSymbolStr.Length - 1;
+                lastBlock = mode;
+                continue;
+            case Match_Mode_ArithmeticSymbol:
+                var operatorSymbol = ConvertOperator(currentChar.ToString());
+                expression.Operators.Add(operatorSymbol);
+                expression.ElementType = ElementType.Expression;
+                lastBlock = mode;
+                continue;
+            case Match_Mode_Function:
+                matchScope = FindEnd('[', endTag, expression.SourceExpressionString, index);
+                //确定函数类型
+                var executeType, function = GetFunctionType(matchScope.ChildrenExpressionString);
+                var functionStr = $"[{matchScope.ChildrenExpressionString}]";
+                //如果是函数，则匹配函数内的表达式,eg: [sum](****)
+                matchScope = FindEnd('(', ')', expression.SourceExpressionString, matchScope.EndIndex + 1);
+                functionStr += $"({matchScope.ChildrenExpressionString})";
+                var functionExp = new Expression
+                {
+                    ElementType = ElementType.Function,
+                    FunctionType = executeType,
+                    Function = function,
+                    FunctionName = executeType.ToString(),
+                    SourceExpressionString = functionStr,
+                    DataString = matchScope.ChildrenExpressionString
+                };
+                expression.ExpressionChildren.Add(functionExp);
+                var paramList = SplitParamObject(matchScope.ChildrenExpressionString);
+                paramList.ForEach(x =>
+                {
+                    var paramExp = new Expression(x);
+                    functionExp.ExpressionChildren.Add(paramExp);
+                });
+                //函数解析完毕后直接从函数后面位置继续
+                index = matchScope.EndIndex;
+                lastBlock = mode;
+                continue;
+            case Match_Mode_Data:
+                if (string.IsnilOrWhiteSpace(currentChar.ToString())) continue;
+                lastBlock = mode;
+                var (str, dataMtachMode) = GetFullData(expression.SourceExpressionString, index, lastBlock);
+                if (!string.IsnilOrWhiteSpace(str))
+                {
+                    //todo 排除转义符长度
+                    if (str.Equals(expression.SourceExpressionString))
+                    {
+                        expression.ElementType = ElementType.Data;
+                        expression.DataString = str;
+                        return;
+                    }
+                    var dataExp = new Expression(str);
+                    if (dataMtachMode == Match_Mode_Scope && currentChar == '-')
+                    {
+                        //如果在Data分支下获取完整数据包含范围描述符号，即小括号，则认为这个负号修饰的是表达式，增加一个负号运算符
+                        expression.Operators.Add(Operator.Negative);
+                        continue;
+                    }
+                    expression.ExpressionChildren.Add(dataExp);
+                }
+                index += str.Length - 1;
+                continue;
+            case Match_Mode_EscapeCharacter:
+                //跳过转义符号
+                index++;
+                lastBlock = mode;
+                continue;
+            default:
+                break;
+        }
+        if !expression.Status{
+            break
+        }
+        // 递归解析子表达式
+        var isOver = exp.ElementType == Element_Data || IsOver(matchScope.ChildrenExpressionString);
+        if !isOver{
+            expressionChildren,_ := CreateExpression(matchScope.ChildrenExpressionString)
+            exp.ExpressionChildren = append(exp.ExpressionChildren,*expressionChildren);
+        }
+        // 跳过已解析的块
+        index = matchScope.EndIndex;
+        lastBlock = mode;
+    }
 }
--- formula_enums.go
+++ formula_enums.go
@@ -3,54 +3,75 @@ package EasyExpression
 type ElementType int
 
 const (
-	Expression ElementType = 0
-	Data       ElementType = 1
-	Function   ElementType = 2
-	Reference  ElementType = 3
+	Element_Expression ElementType = 0
+	Element_Data       ElementType = 1
+	Element_Function   ElementType = 2
+	Element_Reference  ElementType = 3
 )
 
 type Operator int
 
 const (
-	Nil                 Operator = 0
-	And                 Operator = 1
-	Or                  Operator = 2
-	Not                 Operator = 3
-	Plus                Operator = 4
-	Subtract            Operator = 5
-	Multiply            Operator = 6
-	Divide              Operator = 7
-	Mod                 Operator = 8
-	GreaterThan         Operator = 9
-	LessThan            Operator = 10
-	Eq                  Operator = 11
-	UnEquals            Operator = 12
-	GreaterThanOrEquals Operator = 13
-	LessThanOrEquals    Operator = 14
-	Negative            Operator = 15
+	Operator_None                Operator = 0
+	Operator_And                 Operator = 1
+	Operator_Or                  Operator = 2
+	Operator_Not                 Operator = 3
+	Operator_Plus                Operator = 4
+	Operator_Subtract            Operator = 5
+	Operator_Multiply            Operator = 6
+	Operator_Divide              Operator = 7
+	Operator_Mod                 Operator = 8
+	Operator_GreaterThan         Operator = 9
+	Operator_LessThan            Operator = 10
+	Operator_Equals              Operator = 11
+	Operator_UnEquals            Operator = 12
+	Operator_GreaterThanOrEquals Operator = 13
+	Operator_LessThanOrEquals    Operator = 14
+	Operator_Negative            Operator = 15
 )
 
 type FunctionType int
 
 const (
-	None           FunctionType = 0
-	Sum            FunctionType = 1
-	Avg            FunctionType = 2
-	Contains       FunctionType = 3
-	ContainsExcept FunctionType = 4
-	Equals         FunctionType = 5
-	StartWith      FunctionType = 6
-	EndWith        FunctionType = 7
-	Different      FunctionType = 8
-	EDate          FunctionType = 9
-	EODate         FunctionType = 10
-	NowTime        FunctionType = 11
-	TimeToString   FunctionType = 12
-	Round          FunctionType = 13
-	Days           FunctionType = 14
-	Hours          FunctionType = 15
-	Minutes        FunctionType = 15
-	Seconds        FunctionType = 15
-	MillSeconds    FunctionType = 15
-	Customer       FunctionType = 15
+	Function_None           FunctionType = 0
+	Function_Sum            FunctionType = 1
+	Function_Avg            FunctionType = 2
+	Function_Contains       FunctionType = 3
+	Function_ContainsExcept FunctionType = 4
+	Function_Equals         FunctionType = 5
+	Function_StartWith      FunctionType = 6
+	Function_EndWith        FunctionType = 7
+	Function_Different      FunctionType = 8
+	Function_EDate          FunctionType = 9
+	Function_EODate         FunctionType = 10
+	Function_NowTime        FunctionType = 11
+	Function_TimeToString   FunctionType = 12
+	Function_Round          FunctionType = 13
+	Function_Days           FunctionType = 14
+	Function_Hours          FunctionType = 15
+	Function_Minutes        FunctionType = 15
+	Function_Seconds        FunctionType = 15
+	Function_MillSeconds    FunctionType = 15
+	Function_Customer       FunctionType = 15
+)
+
+type MatchMode int
+
+const (
+	//未知模式
+	Match_Mode_Unknown MatchMode = 0
+	//数据
+	Match_Mode_Data MatchMode = 1
+	//逻辑运算符
+	Match_Mode_LogicSymbol MatchMode = 2
+	//算术运算符
+	Match_Mode_ArithmeticSymbol MatchMode = 3
+	//运算范围
+	Match_Mode_Scope MatchMode = 4
+	//函数
+	Match_Mode_Function MatchMode = 5
+	//关系运算符
+	Match_Mode_RelationSymbol MatchMode = 6
+	//转义符
+	Match_Mode_EscapeCharacter MatchMode = 7
 )
