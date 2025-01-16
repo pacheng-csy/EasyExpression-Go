389a28be16d71208577e1bbf663651746ce57fdc
pacheng <pablo.cheng@cn.ey.com>
2025-01-16 15:11:02 +0800

    完善formula action

--- src/formula_action.go
+++ src/formula_action.go
@@ -3,6 +3,8 @@ package EasyExpression
 import (
 	"math"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type FormulaAction struct {
@@ -60,34 +62,64 @@ func (f FormulaAction) Round(values ...any) interface{} {
 /*-----------------String---------------------------*/
 
 func (f FormulaAction) Contains(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	key := values[0].(string)
+	str := values[1].(string)
+	if len(key) == 0 {
+		return float64(1)
+	}
+	if strings.Contains(str, key) {
+		return float64(1)
+	}
+	return float64(0)
 }
 func (f FormulaAction) Excluding(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	key := values[0].(string)
+	str := values[1].(string)
+	if len(key) == 0 {
+		return float64(0)
+	}
+	if strings.Contains(str, key) {
+		return float64(0)
+	}
+	return float64(1)
 }
 func (f FormulaAction) Equals(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	key := values[0].(string)
+	str := values[1].(string)
+	if key == str {
+		return float64(1)
+	}
+	return float64(0)
 }
 func (f FormulaAction) StartWith(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	key := values[0].(string)
+	str := values[1].(string)
+	if len(key) == 0 {
+		return float64(1)
+	}
+	if strings.HasPrefix(str, key) {
+		return float64(1)
+	}
+	return float64(0)
 }
 func (f FormulaAction) EndWith(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	key := values[0].(string)
+	str := values[1].(string)
+	if len(key) == 0 {
+		return float64(1)
+	}
+	if strings.HasSuffix(str, key) {
+		return float64(1)
+	}
+	return float64(0)
 }
 func (f FormulaAction) Different(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	key := values[0].(string)
+	str := values[1].(string)
+	if key == str {
+		return float64(0)
+	}
+	return float64(1)
 }
 
 /*-----------------String---------------------------*/
@@ -95,50 +127,113 @@ func (f FormulaAction) Different(values ...any) interface{} {
 /*-----------------Time---------------------------*/
 
 func (f FormulaAction) EDate(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if date, ok := values[0].(time.Time); ok {
+		value, err := strconv.Atoi(values[1].(string))
+		format := values[2].(string)
+		if err != nil {
+			panic("date parse error")
+		}
+		switch format {
+		case "Y", "y":
+			date.AddDate(value, 0, 0)
+			return date
+		case "M":
+			date.AddDate(0, value, 0)
+			return date
+		case "D", "d":
+			date.AddDate(0, 0, value)
+			return date
+		case "H", "h":
+			date.Add(time.Hour * time.Duration(value))
+			return date
+		case "m":
+			date.Add(time.Minute * time.Duration(value))
+			return date
+		case "S", "s":
+			date.Add(time.Second * time.Duration(value))
+			return date
+		case "F", "f":
+			date.Add(time.Millisecond * time.Duration(value))
+			return date
+		}
+	}
+	panic("date parse error")
 }
 func (f FormulaAction) EODate(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if date, ok := values[0].(time.Time); ok {
+		value, err := strconv.Atoi(values[1].(string))
+		format := values[2].(string)
+		if err != nil {
+			panic("date parse error")
+		}
+		newDate := date.AddDate(0, value, 0)
+		switch format {
+		case "S", "s":
+			return time.Date(newDate.Year(), newDate.Month(), 1, 0, 0, 0, 0, time.UTC)
+		case "E", "e":
+			newDate = time.Date(newDate.Year(), newDate.Month(), 1, 0, 0, 0, 0, time.UTC)
+			newDate = newDate.AddDate(0, 1, 0)
+			newDate = newDate.AddDate(0, 0, -1)
+			return newDate
+		}
+	}
+	panic("EODate execute error")
 }
 func (f FormulaAction) NowTime(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	return time.Now()
 }
 func (f FormulaAction) TimeToString(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if date, ok := values[0].(time.Time); ok {
+		value := values[1].(string)
+		formatting := "2006-01-02 15:04:05"
+		if len(value) > 1 {
+			formatting = value
+			formatting = strings.Replace(formatting, "yyyy", "2006", -1)
+			formatting = strings.Replace(formatting, "YYYY", "2006", -1)
+			formatting = strings.Replace(formatting, "MM", "01", -1)
+			formatting = strings.Replace(formatting, "dd", "02", -1)
+			formatting = strings.Replace(formatting, "DD", "02", -1)
+			formatting = strings.Replace(formatting, "HH", "15", -1)
+			formatting = strings.Replace(formatting, "hh", "15", -1)
+			formatting = strings.Replace(formatting, "mm", "04", -1)
+			formatting = strings.Replace(formatting, "ss", "05", -1)
+			formatting = strings.Replace(formatting, "SS", "05", -1)
+		}
+		return date.Format(formatting)
+	}
+
+	panic("TimeToString execute error")
 }
 
 func (f FormulaAction) Days(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if duration, ok := values[0].(time.Duration); ok {
+		return duration.Hours() / 24
+	}
+	panic("Days execute error")
 }
 func (f FormulaAction) Hours(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if duration, ok := values[0].(time.Duration); ok {
+		return duration.Hours()
+	}
+	panic("Hours execute error")
 }
 func (f FormulaAction) Minutes(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if duration, ok := values[0].(time.Duration); ok {
+		return duration.Minutes()
+	}
+	panic("Minutes execute error")
 }
 func (f FormulaAction) Seconds(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if duration, ok := values[0].(time.Duration); ok {
+		return duration.Seconds()
+	}
+	panic("Seconds execute error")
 }
 func (f FormulaAction) MillSeconds(values ...any) interface{} {
-	result := float64(0)
-	//todo
-	return result
+	if duration, ok := values[0].(time.Duration); ok {
+		return duration.Milliseconds()
+	}
+	panic("MillSeconds execute error")
 }
 
 /*-----------------Time---------------------------*/
---
+++ test/main.go
@@ -0,0 +1,5 @@
+package test
+
+func main() {
+	// This is a test file
+}
